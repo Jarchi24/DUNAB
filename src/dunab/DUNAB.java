7008c4412dfdb45f9f84094c2f1a2d2d2dccf1ed
Javier Luna Diaz <Javier Luna Diaz@LAPTOP-JAVIER>
2025-05-08 15:12:13 -0500

    Carga Inicial

---
+++ src/dunab/DUNAB.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dunab;
+
+/**
+ *
+ * @author Javier Luna Diaz
+ */
+public class DUNAB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
